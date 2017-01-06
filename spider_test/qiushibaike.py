9a1bd95e6474739a05b7f337cbcccbf7b188476d
Xiang <starae86@126.com>
2017-01-06 15:18:18 +0800

    按回车输出每页的第一个信息

--- spider_test/qiushibaike.py
+++ spider_test/qiushibaike.py
@@ -47,7 +47,6 @@ class QSBK:
             re.S)
 
         items = re.findall(pattern, pageCode)
-        # print(items)
         # 存储段子
         pageStories = []
         # 遍历正则匹配的信息
@@ -57,11 +56,10 @@ class QSBK:
             # 不含有图片就把它加到list中
             if not haveImg:
                 replaceBR = re.compile('<br/>')
-                text = re.sub('<br/>', '\n', item[1])
+                text = re.sub(replaceBR, '\n', item[1])
+                # pageStories.append(item)
                 # items[0]是作者信息,items[1]是段子内容，items[3]是赞数
-                pageStories.append(item)
-                # pageStories.append(text)
-                # pageStories.append(items[3])
+                pageStories.append([item[0].strip(), text.strip(), item[3].strip()])
                 return pageStories
 
     # 加载并提取页面内容，加入到列表中
@@ -74,13 +72,13 @@ class QSBK:
                 # 将该页的段子存放在全局list中
                 if pageStories:
                     self.stories.append(pageStories)
-                    print(len(self.stories))
+                    # print(len(self.stories))长度始终为1
                     # 索引加一
                     self.pageIndex += 1
 
     # 调用该方法，每次敲回车输出一个段子
     def getOneStroy(self, pageStories, page):
-        # 便利一页的段子
+        # 遍历一页的段子
         for story in pageStories:
             # 等待用户输入
             Input = input()
@@ -90,7 +88,7 @@ class QSBK:
             if Input == 'q':
                 self.enable = False
                 return
-            print('第%d页\t发布人:%s\t赞数:%s\n%s' % (page, story[0], story[3], story[1]))
+            print('第%d页\t发布人:%s\t赞数:%s\n%s' % (page, story[0], story[2], story[1]))
 
     # 开始方法
     def start(self):
@@ -99,23 +97,18 @@ class QSBK:
         self.enable = True
         # 先加载一页内容
         self.loadPage()
-        # print(self.stories)
         # 局部变量，控制当前读到了第几页
         nowPage = 0
         while self.enable:
             if len(self.stories) > 0:
                 # 从全局list中获取第一页的段子
-                pageStories = self.stories[0]
+                pageStories = self.stories.pop(0)
                 # 当前读取到的页数+1
                 nowPage += 1
-                # 将全局list中的第一个元素删除，因为已经取出
-                del self.stories[0]
                 # 输出该页的段子内容
                 self.getOneStroy(pageStories, nowPage)
-                # for story in self.stories:
-                #     nowPage+=1
-                #     self.getOneStroy(story,nowPage)
 
 
-spider = QSBK()
-spider.start()
+if __name__ == '__main__':
+    spider = QSBK()
+    spider.start()
